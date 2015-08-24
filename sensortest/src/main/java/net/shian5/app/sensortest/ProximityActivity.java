e9f4e2c66be908f521e5e459b75efe9fa79839d8
shian <shian5@gmail.com>
2015-08-24 15:52:58 +0800

    show value

--- sensortest/src/main/java/net/shian5/app/sensortest/ProximityActivity.java
+++ sensortest/src/main/java/net/shian5/app/sensortest/ProximityActivity.java
@@ -1,19 +1,47 @@
 package net.shian5.app.sensortest;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class ProximityActivity extends Activity {
+public class ProximityActivity extends Activity implements SensorEventListener {
+
+    private SensorManager mgr;
+    private Sensor proximity;
+    private TextView disText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proximity);
+
+        this.disText = (TextView) findViewById(R.id.disText);
+
+        this.mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        // 取得接近感應器
+        this.proximity = this.mgr.getDefaultSensor(Sensor.TYPE_PROXIMITY);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 在這裏註冊
+        this.mgr.registerListener(this, this.proximity,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 在這裏解除註冊
+        this.mgr.unregisterListener(this, this.proximity);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +63,16 @@ public class ProximityActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float distance = event.values[0]; // 目前的距離
+        this.disText.setText(Float.toString(distance));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
